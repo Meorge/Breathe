4a1735ed933d0403b74b03e2991b1f39497115bc
Malcolm Anderson <malcolminyo@gmail.com>
2020-09-29 21:49:01 -0700

    initial commit

---
+++ Assets/Resources/BreatheThing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shapes;
+
+public class BreatheThing : MonoBehaviour
+{
+    public List<Disc> discs = new List<Disc>();
+
+    public float discAlpha = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Disc d in discs) {
+            d.SetAlpha(discAlpha);
+        }
+    }
+}
---
+++ Assets/Resources/PlaceOnAngle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceOnAngle : MonoBehaviour
+{
+    public float angle = 0f;
+
+    public float radius = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float radAngle = angle * Mathf.Deg2Rad;
+        Vector3 pos = new Vector3(Mathf.Cos(radAngle) * radius, Mathf.Sin(radAngle) * radius, transform.localPosition.z);
+        transform.localPosition = pos;
+    }
+}
